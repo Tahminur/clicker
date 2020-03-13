7811b664ad3ab96d85412886742d0250768fac00
Tahminur Rahman <trahman1@pride.hofstra.edu>
2020-03-12 20:25:15 -0400

    Initial Commit

---
+++ Clicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clicker
+//
+//  Created by Tahminur Rahman on 3/12/20.
+//  Copyright © 2020 Tahminur Rahman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
